7b4dcb08ffbb9ce3d1d9b3ccd85f607045041b94
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-28 22:33:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  if block_given?
+   x = 0 
+   new_array = []
+   while x < array.length do
+      new_array.push(yield(array[x]))
+      x +=1
+   end
+  end
+  new_array
+end
