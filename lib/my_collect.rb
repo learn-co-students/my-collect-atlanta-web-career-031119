ddf230a28e54726d2de7c962e6cf23f3ab8d5d3a
Travis Martin <travisemerymartin@gmail.com>
2019-03-04 18:09:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if block_given?
+    i=0
+    while i<array.length
+      x = yield array[i]
+      array[i] = x
+      i+=1
+    end
+    return array
+  else
+    return array
+  end
+end
