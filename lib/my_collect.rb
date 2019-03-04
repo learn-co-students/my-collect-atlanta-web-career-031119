df19174f392c73ec2f4f36979a17127a4a5920bf
Travis Martin <travisemerymartin@gmail.com>
2019-03-04 18:15:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,12 @@ def my_collect(array)
   if block_given?
     i=0
     while i<array.length
-      x = yield array[i]
-      array[i] = x
+      new_array=[]
+      x=yield array[i]
+      new_array<<x
       i+=1
     end
-    return array
+    return new_array
   else
     return array
   end
